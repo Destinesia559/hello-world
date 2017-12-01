3ef2c9b89190e311ad9534b0447f901d50a51a0e
Destinesia559 <34155379+Destinesia559@users.noreply.github.com>
2017-12-01 19:51:22 +0800

    Update Test.java

--- src/com/biubiubiu/po/Test.java
+++ src/com/biubiubiu/po/Test.java
@@ -2,6 +2,7 @@ package com.biubiubiu.po;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("?????");
+		System.out.println("123");
+		System.out.println("321");
 	}
 }
